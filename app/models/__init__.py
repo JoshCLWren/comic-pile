1e8f9b4de901e6d90cca1e7134bb1c558e74a71e
Josh Wren <joshisplutar@gmail.com>
2026-01-07 12:59:12 -0600

    fix: add Snapshot model to __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,8 +3,9 @@
 from app.models.event import Event
 from app.models.session import Session
 from app.models.settings import Settings
+from app.models.snapshot import Snapshot
 from app.models.task import Task
 from app.models.thread import Thread
 from app.models.user import User
 
-__all__ = ["User", "Thread", "Session", "Event", "Task", "Settings"]
+__all__ = ["User", "Thread", "Session", "Event", "Task", "Settings", "Snapshot"]
