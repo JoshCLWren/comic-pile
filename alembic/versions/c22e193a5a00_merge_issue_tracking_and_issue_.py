a37a824990cb21795f231993a634c5237d75a47f
Josh Wren <joshisplutar@gmail.com>
2026-03-01 09:20:25 -0600

    migration fix

--- alembic/versions/c22e193a5a00_merge_issue_tracking_and_issue_.py
+++ alembic/versions/c22e193a5a00_merge_issue_tracking_and_issue_.py
@@ -5,17 +5,15 @@ Revises: 9f97c046466f, a4c9d8e7f6b1
 Create Date: 2026-03-01 09:16:47.923174
 
 """
-from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
+from collections.abc import Sequence
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'c22e193a5a00'
-down_revision: Union[str, Sequence[str], None] = ('9f97c046466f', 'a4c9d8e7f6b1')
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+revision: str = "c22e193a5a00"
+down_revision: str | Sequence[str] | None = ("9f97c046466f", "a4c9d8e7f6b1")
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
