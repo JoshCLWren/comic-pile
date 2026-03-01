68e86b4f3b0bc6aae292fc29124ff63a334850a1
Josh Wren <joshisplutar@gmail.com>
2026-03-01 09:18:04 -0600

    Add merge migration for divergent alembic heads

---
+++ alembic/versions/c22e193a5a00_merge_issue_tracking_and_issue_.py
@@ -0,0 +1,28 @@
+"""merge issue tracking and issue dependencies heads
+
+Revision ID: c22e193a5a00
+Revises: 9f97c046466f, a4c9d8e7f6b1
+Create Date: 2026-03-01 09:16:47.923174
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'c22e193a5a00'
+down_revision: Union[str, Sequence[str], None] = ('9f97c046466f', 'a4c9d8e7f6b1')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
