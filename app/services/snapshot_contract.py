684ac56c3889e228a8d396cb2731a51f73f5d9ae
JoshCLWren <joshisplutar@gmail.com>
2026-08-01 22:36:05 -0500

    Share undo snapshot contract constants

---
+++ app/services/snapshot_contract.py
@@ -0,0 +1,7 @@
+"""Shared wire-contract constants for undo snapshot payloads."""
+
+SNAPSHOT_VERSION_KEY = "_version"
+SNAPSHOT_VERSION = 2
+QUEUE_CHANGES_KEY = "_queue_changes"
+BLOCKED_CHANGES_KEY = "_blocked_changes"
+USES_ISSUE_TRACKING_KEY = "uses_issue_tracking"
