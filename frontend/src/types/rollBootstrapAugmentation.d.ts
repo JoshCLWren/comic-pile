67f6a339f8fce1e77001b32eb9d0fb6412e3fae6
JoshCLWren <joshisplutar@gmail.com>
2026-08-03 06:48:59 -0500

    Remove temporary Roll bootstrap type bridge

--- frontend/src/types/rollBootstrapAugmentation.d.ts
+++
@@ -1,7 +0,0 @@
-import type { RollBootstrapThread as RollBootstrapThreadShape } from './rollBootstrap'
-
-export {}
-
-declare module './index' {
-  interface RollBootstrapThread extends RollBootstrapThreadShape {}
-}
