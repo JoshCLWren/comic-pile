33bf00a445d44961c88f2a299ac43262ede96fdb
JoshCLWren <joshisplutar@gmail.com>
2026-08-03 07:13:33 -0500

    Remove retired Collections test declaration

--- frontend/src/contexts/CollectionContext.d.ts
+++
@@ -1,9 +0,0 @@
-/** Test-only declaration retained while RollPage tests finish migrating off Collections. */
-export interface RetiredCollectionContextValue {
-  collections: never[]
-  activeCollectionId: null
-  setActiveCollectionId: (value: null) => void
-  isLoading: boolean
-}
-
-export declare function useCollections(): RetiredCollectionContextValue
