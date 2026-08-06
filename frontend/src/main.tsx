a2a54fdbf8cb5680400a06692dab6abb459b0482
JoshCLWren <joshisplutar@gmail.com>
2026-08-05 23:31:10 -0500

    Keep a visible recovery surface after mobile resume

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import AppErrorBoundary from './components/AppErrorBoundary'
+import ResumeRecovery from './components/ResumeRecovery'
 import { SessionProvider } from './contexts/SessionContext'
 import { ToastProvider } from './contexts/ToastProvider'
 import './index.css'
@@ -13,11 +15,15 @@ if (!rootElement) {
 
 createRoot(rootElement).render(
   <StrictMode>
-    <SessionProvider>
-      <ToastProvider>
-        <App />
-      </ToastProvider>
-    </SessionProvider>
+    <AppErrorBoundary>
+      <ResumeRecovery>
+        <SessionProvider>
+          <ToastProvider>
+            <App />
+          </ToastProvider>
+        </SessionProvider>
+      </ResumeRecovery>
+    </AppErrorBoundary>
   </StrictMode>,
 )
 
