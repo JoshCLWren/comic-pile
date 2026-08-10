b7926004986f3eb1a40e7c9ca56bc56db7719755
JoshCLWren <joshisplutar@gmail.com>
2026-08-10 08:40:54 -0500

    Add roll prerequisite switch client

--- frontend/src/services/rollBootstrapApi.ts
+++ frontend/src/services/rollBootstrapApi.ts
@@ -1,6 +1,12 @@
 import api from './api'
-import type { RollBootstrapResponse } from '../types/rollBootstrap'
+import type {
+  RollBootstrapResponse,
+  RollPrerequisiteSwitchRequest,
+  RollPrerequisiteSwitchResponse,
+} from '../types/rollBootstrap'
 
 export const rollBootstrapApi = {
   get: () => api.get<RollBootstrapResponse>('/v1/roll/bootstrap'),
+  switchPrerequisite: (request: RollPrerequisiteSwitchRequest) =>
+    api.post<RollPrerequisiteSwitchResponse>('/v1/roll/switch-prerequisite', request),
 }
